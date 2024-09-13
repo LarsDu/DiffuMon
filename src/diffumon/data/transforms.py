a3d2ea61995eb4871e2d365d04d0165b5953d9ee
Larry Du <larrydu88@gmail.com>
2024-09-12 23:47:08 -0700

    Device handling fix in reverse_transform.

--- src/diffumon/data/transforms.py
+++ src/diffumon/data/transforms.py
@@ -51,7 +51,7 @@ def reverse_transform() -> Callable:
     return Compose(
         [
             Lambda(lambda t: (t + 1) / 2 * 255.0),  # Rescale to [0, 255]
-            Lambda(lambda t: t.permute(1, 2, 0).numpy()),  # CHW to HWC
+            Lambda(lambda t: t.permute(1, 2, 0).cpu().numpy()),  # CHW to HWC
             Lambda(lambda t: t.numpy().astype(np.uint8)),  # Convert to numpy image
             ToPILImage(),
         ]
