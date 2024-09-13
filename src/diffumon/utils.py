bcd76ac7f7bede30c4538d62ccf5c2aef86412bc
Larry Du <larrydu88@gmail.com>
2024-09-12 22:32:42 -0700

    Tell what device is being used.

--- src/diffumon/utils.py
+++ src/diffumon/utils.py
@@ -2,4 +2,6 @@ import torch
 
 
 def get_device() -> torch.device:
-    return torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f"Using device: {device}")
+    return device
